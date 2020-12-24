7c02e09cafb4444406c37a8afdd4f64ea18eaa0d
nomis51 <nomis51@github.com>
2020-12-24 11:48:12 -0500

    Add clipboard listener

---
+++ src/Toucan.Core/ClipboardListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toucan.Core {
+    public class ClipboardListener {
+        public delegate void NewClipboardText(string text);
+        public event NewClipboardText OnNewClipboardText;
+
+        public ClipboardListener() {
+            Listen();
+        }
+
+        private async void Listen() {
+            while (true) {
+                await Task.Delay(500);
+
+                string text = Clipboard.GetText();
+
+                if (!string.IsNullOrEmpty(text)) {
+                    OnNewClipboardText(text);
+                }
+            }
+        }
+    }
+}
