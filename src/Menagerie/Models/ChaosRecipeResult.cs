92e0c61e5d99b05d36b49e8a6b618dcced7dbdb0
nomis51 <nomis51@users.noreply.github.com>
2021-05-28 16:34:31 -0400

    FIX: chaos recipe not displaying data

--- src/Menagerie/Models/ChaosRecipeResult.cs
+++ src/Menagerie/Models/ChaosRecipeResult.cs
@@ -4,33 +4,33 @@
     {
         public int NbGloves { get; set; } = 0;
 
-        public bool NeedGloves { get; set; }
+        public bool NeedGloves { get; set; } = true;
 
         public int NbHelmets { get; set; } = 0;
 
-        public bool NeedHelmets { get; set; }
+        public bool NeedHelmets { get; set; } = true;
 
         public int NbBoots { get; set; } = 0;
 
-        public bool NeedBoots { get; set; }
+        public bool NeedBoots { get; set; } = true;
 
         public int NbBelts { get; set; } = 0;
 
-        public bool NeedBelts { get; set; }
+        public bool NeedBelts { get; set; } = true;
 
         public int NbBodyArmours { get; set; } = 0;
 
-        public bool NeedBodyArmours { get; set; }
+        public bool NeedBodyArmours { get; set; } = true;
 
         public int NbAmulets { get; set; } = 0;
 
-        public bool NeedAmulets { get; set; }
+        public bool NeedAmulets { get; set; } = true;
 
         public int NbRings { get; set; } = 0;
 
         public int NbRingSets { get; set; }
 
-        public bool NeedRings { get; set; }
+        public bool NeedRings { get; set; } = true;
 
         public int Nb2HWeapons { get; set; } = 0;
         public int Nb1HWeapons { get; set; } = 0;
@@ -38,7 +38,7 @@
 
         public int NbWeaponSets { get; set; }
 
-        public bool NeedWeapons { get; set; }
+        public bool NeedWeapons { get; set; } = true;
 
         public int NbSets { get; set; }
     }
