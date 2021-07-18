f5bc1450a57987539c4c2d679a766621ae9e8460
nomis51 <nomis51@users.noreply.github.com>
2021-07-17 20:05:10 -0400

    FIX: PoE Process not found

--- src/Menagerie.Core/Services/PoeWindowService.cs
+++ src/Menagerie.Core/Services/PoeWindowService.cs
@@ -41,6 +41,7 @@ namespace Menagerie.Core.Services
 
         private readonly List<string> _poeProcesses = new List<string>()
         {
+            "PathOfExile",
             "PathOfExile_x64",
             "PathOfExile_x64Steam"
         };
