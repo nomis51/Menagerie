c7d1fe8468ef0f8789cc4f584f43798f187f5fd7
nomis51 <nomis51@users.noreply.github.com>
2021-07-20 09:34:12 -0400

    FIX: json property names of models

--- src/Menagerie.Core/Models/CloudData/AiAnalyzes.cs
+++ src/Menagerie.Core/Models/CloudData/AiAnalyzes.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using Menagerie.Core.Models.ML;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Menagerie.Core.Models.CloudData
 {
     public class AiAnalyzes
     {
+        [JsonProperty("ai_predictions")]
         public PredictionResponse AiPredictions { get; set; }
         public List<PredictionImage> Images { get; set; } = new();
     }
