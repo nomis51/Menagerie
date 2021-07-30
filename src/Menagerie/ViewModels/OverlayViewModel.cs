59b65647cf499be81892305e254adca60458bd88
nomis51 <nomis51@users.noreply.github.com>
2021-07-30 09:34:25 -0400

    FIX: clearing outgoing offers #29

--- src/Menagerie/ViewModels/OverlayViewModel.cs
+++ src/Menagerie/ViewModels/OverlayViewModel.cs
@@ -442,7 +442,7 @@ namespace Menagerie.ViewModels
 
             if (overflowOffers.Count <= 0 || offers.Value.Count >= 8) return;
 
-            while (offers.Value.Count < 8)
+            while (offers.Value.Count < 8 && offers.Value.Count > 0)
             {
                 offers.Value.Add(overflowOffers.Dequeue());
             }
@@ -704,7 +704,7 @@ namespace Menagerie.ViewModels
             if (overflowOffers.Count == 0) return;
 
 
-            while (offers.Value.Count < 8)
+            while (offers.Value.Count < 8 && offers.Value.Count > 0)
             {
                 offers.Value.Add(overflowOffers.Dequeue());
             }
