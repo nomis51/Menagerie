56378cccf9c7dd9ff5edb389e4ce5124862d39f0
nomis51 <nomis51@users.noreply.github.com>
2022-08-18 11:20:33 -0300

    Adjust bulk trade models

--- src/Menagerie.Application/DTOs/BulkTradeItemDto.cs
+++ src/Menagerie.Application/DTOs/BulkTradeItemDto.cs
@@ -5,11 +5,16 @@ public class BulkTradeItemDto
     public double PayAmount { get; set; }
     public string PayCurrency { get; set; }
     public Uri PayCurrencyImage { get; set; }
-    
+    public string PayNativeWhisperTemplate { get; set; }
+
     public double GetAmount { get; set; }
     public string GetCurrency { get; set; }
     public Uri GetCurrencyImage { get; set; }
-    
-    public string Whisper { get; set; }
+    public string GetNativeWhisperTemplate { get; set; }
+
+    public string WhisperTemplate { get; set; }
     public string Player { get; set; }
+    public string LastCharacterName { get; set; }
+
+    public string Whisper => string.Format(WhisperTemplate, string.Format(PayNativeWhisperTemplate, PayAmount), string.Format(GetNativeWhisperTemplate, GetAmount));
 }
--- src/Menagerie.Shared/Models/Poe/BulkTrade/BulkTradeListingAccount.cs
+++ src/Menagerie.Shared/Models/Poe/BulkTrade/BulkTradeListingAccount.cs
@@ -4,7 +4,6 @@ public class BulkTradeListingAccount
 {
     public string Name { get; set; }
     public string LastCharacterName { get; set; }
-    public BulkTradeListingAccountOnline Online { get; set; }
     public string Language { get; set; }
     public string Realm { get; set; }
 }
