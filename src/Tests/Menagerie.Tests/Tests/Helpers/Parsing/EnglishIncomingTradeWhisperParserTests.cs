c92c82bbdf1d62487eb7164e508351898702c2fb
nomis51 <nomis51@users.noreply.github.com>
2025-11-01 09:47:26 -0400

    Add tests for invalid whispers

--- src/Tests/Menagerie.Tests/Tests/Helpers/Parsing/EnglishIncomingTradeWhisperParserTests.cs
+++ src/Tests/Menagerie.Tests/Tests/Helpers/Parsing/EnglishIncomingTradeWhisperParserTests.cs
@@ -9,6 +9,16 @@ public class EnglishIncomingTradeWhisperParserTests
     #region Members
 
     private readonly EnglishIncomingTradeWhisperParser _sut = new();
+    private readonly ITestOutputHelper _output;
+
+    #endregion
+
+    #region Constructors
+
+    public EnglishIncomingTradeWhisperParserTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
 
     #endregion
 
@@ -19,12 +29,26 @@ public class EnglishIncomingTradeWhisperParserTests
     public void Parse_ShouldReturnNull(string input)
     {
         // Arrange
+        _output.WriteLine($"Testing input: {input}");
 
         // Act
         var result = _sut.Parse(input);
 
         // Assert
         result.ShouldBeNull();
+    }
+
+    [Theory]
+    [ClassData(typeof(ClientLogValidIncomingTradeLines))]
+    public void Parse_ShouldReturnTrade(string input)
+    {
+        // Arrange
+
+        // Act
+        var result = _sut.Parse(input);
+
+        // Assert
+        result.ShouldNotBeNull();
     }
 
     #endregion
